255ae176757f4f23bc7923ce29ec0e4ff4ad5693
Aliffiro <fathf48@gmail.com>
2019-03-31 20:14:49 +0700

    Init Awal
    31 Maret 2019

---
+++ app/src/main/java/sidev/techdev/siitclub/MainActivity.java
@@ -0,0 +1,13 @@
+package sidev.techdev.siitclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
