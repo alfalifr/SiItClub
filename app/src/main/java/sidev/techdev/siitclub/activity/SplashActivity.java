9b101eb75030a5f8e8bae8514e6411ed8079d1f1
Yohanes Jose Ariawan <joseariawan96@gmail.com>
2019-05-24 11:03:56 +0700

    Splash & login
    Buat halaman splash
    ganti background color login

---
+++ app/src/main/java/sidev/techdev/siitclub/activity/SplashActivity.java
@@ -0,0 +1,13 @@
+package sidev.techdev.siitclub.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
